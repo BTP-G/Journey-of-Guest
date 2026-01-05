f1872c97c2867173c29c197e987f123da93b6c6c
BTP <3503922521@qq.com>
2026-01-05 21:17:59 +0800

    1

--- Assets/Scripts/BootstrapManager.cs
+++ Assets/Scripts/BootstrapManager.cs
@@ -1,11 +1,9 @@
 using Cysharp.Threading.Tasks;
-using EditorAttributes;
 using GuestUnion.UI;
 using System;
 using System.Collections.Generic;
 using Unity.Services.Core;
 using UnityEngine;
-using UnityEngine.LowLevel;
 using UnityEngine.SceneManagement;
 using VContainer;
 using VContainer.Unity;
@@ -14,6 +12,7 @@ namespace JoG {
 
     public class BootstrapManager : MonoBehaviour {
         public PopupManager PopupManager;
+
         private async void Awake() {
             using var scope = PopupManager.PopupLoader();
             while (UnityServices.State != ServicesInitializationState.Initialized) {
@@ -29,24 +28,6 @@ namespace JoG {
                 await startable.InitializeAsync();
             }
             await SceneManager.LoadSceneAsync(1);
-        }
-
-        private void Start() {
-            //var root = PlayerLoop.GetDefaultPlayerLoop();
-            //print(PrintSystem(root, 0));
-        }
-
-        private string PrintSystem(in PlayerLoopSystem system, int depth) {
-            var msg = "";
-            var indent = new string(' ', depth * 2);
-            msg += $"{indent}{system.type?.Name ?? "ROOT"}\n";
-
-            if (system.subSystemList != null) {
-                foreach (var sub in system.subSystemList) {
-                    msg += PrintSystem(sub, depth + 1);
-                }
-            }
-            return msg;
         }
     }
 }
